ffe211e44c52d547915e36f24df0b715681b03b8
Thiago <tmelo.mat@gmail.com>
2019-06-22 13:50:15 -0300

    Add files via upload

--- lali-mm.py
+++ lali-mm.py
@@ -4,22 +4,23 @@
 from PIL import Image, ImageDraw, ImageFont
 import os, sys, textwrap
 
+''' replace the file below to use as constant input image '''
+imageFile = "lali-mm.jpg"
+
 if len(sys.argv) == 4:
     imageFile = sys.argv[-1]
-    if os.path.exists(imageFile):
-        print('creating meme on "%s"' % imageFile)
-    else:
+    if not os.path.exists(imageFile):
         print('** either the file "%s" is missing or is not an image' % imageFile)
+        sys.exit()
+
+if len(sys.argv) >= 3:
+    msg = sys.argv[1:]
+    top_msg = msg[0].decode("utf-8")
+    bottom_msg = msg[1].decode("utf-8")
 else:
-    ''' replace the file below to use as constant input image '''
-    imageFile = "lali-mm.jpg"
-    print('creating meme on "%s"' % imageFile)
+    top_msg = 'top text'
+    bottom_msg = 'bottom text'
 
-
-msg = sys.argv[1:]
-
-top_msg = msg[0].decode("utf-8")
-bottom_msg = msg[1].decode("utf-8")
 top_box = textwrap.wrap(top_msg, width=25)
 bottom_box = textwrap.wrap(bottom_msg, width=20)
 
@@ -49,7 +50,7 @@ for line in top_box:
     draw.text((x, y), line, font=font, fill=fillcolor)
     current_h += h + pad
 
-current_h, pad = 0.80 * MAX_H, 10
+current_h, pad = 0.85 * MAX_H, 10
 for line in bottom_box:
     w, h = draw.textsize(line, font=font)
     x, y = (MAX_W - w) / 2, current_h
@@ -66,11 +67,13 @@ for line in bottom_box:
     draw.text((x, y), line, font=font, fill=fillcolor)
     current_h += h + pad
 
-num_mm = 1
+num_mm = []
 prefix = os.path.splitext(imageFile)[0]
 for filename in os.listdir('.'):
         if filename.startswith(prefix + '-') and filename.endswith('.jpg'):
-            num_mm += 1
+            num_mm.append(int(os.path.splitext(filename.split(prefix + '-')[1])[0]))
+            print filename
+next_mm = max(num_mm)+1
 
-im.save('%s-%02d.jpg' % (prefix, num_mm))
-print('%s-%02d.jpg created' % (prefix, num_mm))
+im.save('%s-%02d.jpg' % (prefix, next_mm))
+print('creating meme from "%s" and saved on "%s-%02d.jpg"' % (imageFile, prefix, next_mm))
